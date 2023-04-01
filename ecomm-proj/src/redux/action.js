b6f89621b0e22fa4c845fe3e08758ad829786144
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-04-01 13:32:17 +0530

    L-7

--- ecomm-proj/src/redux/action.js
+++ ecomm-proj/src/redux/action.js
@@ -1,7 +1,8 @@
+import { ADD_TO_CART } from "./constant";
 export const addToCart = (data) => {
-  console.log("Action called", data);
+  console.log("Action is called", data);
   return {
-    type: "Add_TO_CART",
+    type: ADD_TO_CART,
     data: data,
   };
 };
---
+++ ecomm-proj/src/redux/reducer.js
@@ -0,0 +1,26 @@
+import { ADD_TO_CART, REMOVE_TO_CART } from "./constant";
+
+export const cartData = (data = [], action) => {
+  //   if (action.type === ADD_TO_CART) {
+  //     console.log("reducer action called " + action);
+  //     return action.data;
+  //   } else {
+  //     return "no action matched";
+  //   }
+
+  switch (action.type) {
+    case ADD_TO_CART:
+      // add to cart logic
+      console.log("ADD_TO_CART CONDITION" + action);
+      return 1 + 1;
+
+    case REMOVE_TO_CART:
+      // Remove from cart
+      console.log("REMOVE_TO_CART CONDITION" + action);
+      return 1 - 1;
+
+    default:
+      // no case match
+      return "No action called";
+  }
+};
